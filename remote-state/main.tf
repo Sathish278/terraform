211ddabf68049c76efe79eba50a2189b6ab93674
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-05-11 12:23:16 +0530

    final

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "db" {
 
     ami = "ami-090252cbe067a9e58"
-    vpc_security_group_ids = ["sg-074c6f8bbe306fc73"]
+    vpc_security_group_ids = ["sg-08bef055e77f836f7"]
     instance_type = "t3.micro"
 
     tags = {
         Name = "db"
     }
 }
+
